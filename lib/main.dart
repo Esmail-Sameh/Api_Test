28778e3937e566826c8862c23367c7af43eb5283
Esmail <semoa5488@gmail.com>
2023-10-04 07:29:36 -0700

    Basic files

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:apiget/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
